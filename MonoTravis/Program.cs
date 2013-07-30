3d5b83e5d714d00dc7b29afe3ce7164242d95707
icattlecoder <icattlecoder@gmail.com>
2013-07-30 22:08:25 +0800

    add files

---
+++ MonoTravis/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MonoTravis
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int a = 1;
+			int b = 2;
+			Console.WriteLine ("Hello World!{0}",a+b);
+		}
+	}
+}
